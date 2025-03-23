6dd0566657ba3233cb3d4bc28e01fc36cb4bb869
MiguelMunoz555 <m.munoz12@uniandes.edu.co>
2025-03-23 18:46:49 -0500

    Proyecto Numero 4

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
-from app import create_app
-from app.config.config import Config  # Asegúrate de importar Config
+import os
+from app.config.routes import app  # Importa la instancia de la app
 
-app = create_app(Config)  # Pasa la configuración correctamente
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
