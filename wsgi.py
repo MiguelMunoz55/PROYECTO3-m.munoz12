671bdd602e88d58bd0bb63eb7def50c83369fa20
MiguelMunoz555 <m.munoz12@uniandes.edu.co>
2025-03-23 18:52:38 -0500

    Proyecto Numero 4

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,20 @@
 import os
-from app.config.routes import app  # Importa la instancia de la app
+from app.config.routes import app
+from app.config.db import db
+from app.models.usuario import Usuario
+
+with app.app_context():
+    if not Usuario.query.first():
+        admin = Usuario(username="admin", password="1234", es_admin=True, es_empleado=False)
+        empleado = Usuario(username="empleado", password="5678", es_admin=False, es_empleado=True)
+        usuario = Usuario(username="usuario", password="9012", es_admin=False, es_empleado=False)
+
+        db.session.add_all([admin, empleado, usuario])
+        db.session.commit()
+
+        print("✅ Usuarios insertados correctamente")
+    else:
+        print("⚠️ Usuarios ya existen en la base de datos")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
