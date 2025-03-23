ddee7f355487a707dc12166968ebe65e45e90acd
MiguelMunoz555 <m.munoz12@uniandes.edu.co>
2025-03-23 18:56:50 -0500

    Proyecto Numero 4

--- app/config/config.py
+++ app/config/config.py
@@ -3,3 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///fallback.db")  # Usa SQLite si la variable está vacía
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY", "clave_secreta_por_defecto")
