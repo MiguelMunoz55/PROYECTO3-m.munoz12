ee4ad3a7775f8974d053acded0c9e5c411669717
MiguelMunoz555 <m.munoz12@uniandes.edu.co>
2025-03-23 18:33:55 -0500

    Proyecto Numero 4

--- app/__init__.py
+++ app/__init__.py
@@ -16,19 +16,4 @@ def create_app(config):
         from app.models.usuario import Usuario
         db.create_all()
 
-        # ✅ Importa y registra los blueprints aquí dentro del contexto
-        from app.controllers.home_controller import home_blueprint
-        from app.controllers.auth_controller import auth_blueprint
-        from app.controllers.admin_controller import admin_blueprint
-        from app.controllers.user_controller import user_blueprint
-        from app.controllers.empleado_controller import empleado_blueprint
-        from app.controllers.api_controller import api_blueprint
-
-        app.register_blueprint(home_blueprint)
-        app.register_blueprint(auth_blueprint)
-        app.register_blueprint(admin_blueprint)
-        app.register_blueprint(empleado_blueprint)
-        app.register_blueprint(user_blueprint)
-        app.register_blueprint(api_blueprint)
-
     return app
--- app/config/config.py
+++ app/config/config.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv(override=True)
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///fallback.db")  # Usa SQLite si la variable está vacía
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
