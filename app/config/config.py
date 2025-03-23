d9e672afd462914350099970e1dbbc14e76ad266
MiguelMunoz555 <m.munoz12@uniandes.edu.co>
2025-03-23 17:58:48 -0500

    Proyecto Numero 4

--- app/config/config.py
+++ app/config/config.py
@@ -1,13 +1,5 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv(override=True)
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///fallback.db")  # Usa SQLite si la variable está vacía
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
-
-# 🔎 Imprimir valores para verificar que se cargan correctamente
-print("DATABASE_URI en config.py:", os.getenv("DATABASE_URI"))
-print("SECRET_KEY en config.py:", os.getenv("SECRET_KEY"))
